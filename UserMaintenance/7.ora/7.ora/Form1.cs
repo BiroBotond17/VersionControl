b7e38d38bbf39f4ce173cee1ff5daaa489091ef6
Botond Biró <botond.biro@stud.uni-corvinus.hu>
2022-11-11 12:15:57 +0100

    4.feladat

--- UserMaintenance/7.ora/7.ora/Form1.cs
+++ UserMaintenance/7.ora/7.ora/Form1.cs
@@ -12,9 +12,19 @@ namespace _7.ora
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
         public Form1()
         {
             InitializeComponent();
+            
+            dataGridView1.DataSource = Ticks;
+            Ticks = context.Tick.ToList();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
