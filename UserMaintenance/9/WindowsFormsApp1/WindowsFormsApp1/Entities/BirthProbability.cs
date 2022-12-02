3e9e524100479e61e205e9a27e879d86792729f9
Botond Biró <botond.biro@stud.uni-corvinus.hu>
2022-12-02 12:31:08 +0100

    1

---
+++ UserMaintenance/9/WindowsFormsApp1/WindowsFormsApp1/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Entities
+{
+    public class BirthProbability
+    {
+        public int Kor { get; set; }
+        public int gyermekek { get; set; }
+        public double valsz { get; set; }
+    }
+}
---
+++ UserMaintenance/9/WindowsFormsApp1/WindowsFormsApp1/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Entities
+{
+    public class DeathProbability
+    {
+        public int nem { get; set; }
+        public int kor { get; set; }
+        public double halalvalsz { get; set; }
+    }
+}
