1d79bf5e65e973c94a4b3a76c4fd1a5da6d5900d
Botond Biró <botond.biro@stud.uni-corvinus.hu>
2022-12-09 12:16:22 +0100

    2

--- UserMaintenance/9/week10/week10/Form1.cs
+++ UserMaintenance/9/week10/week10/Form1.cs
@@ -27,6 +27,7 @@ namespace week10
             //gc.AddPlayer();
             // gc.Start(true);
             gc.GameOver += Gc_GameOver;
+            Brain winnerBreain = null;
             for (int i = 0; i < populationSize; i++)
             {
                 gc.AddPlayer(nbrOfSteps);
@@ -44,6 +45,43 @@ namespace week10
             label1.Text = string.Format(
                 "{0}. generáció",
                 generation);
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+            gc.ResetCurrentLevel();
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gc.ResetCurrentLevel();
+            gc.AddPlayer(winnerBrain.Clone());
+            gc.AddPlayer();
+            ga.Focus();
+            gc.Start(true);
         }
     }
 }
